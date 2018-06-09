cacc096ab5bde8c193d10d9889d71c2058f36892
krishna <33161711+skmurali@users.noreply.github.com>
2018-06-09 13:54:23 -0700

    Create ApplicationInitializer.java

---
+++ ApplicationInitializer.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletContext ;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer 
+{
+	 @Override
+	 protected Class < ? > [] getRootConfigClasses() 
+	 {
+	  return new Class[] {
+	   ApplicationConfiguration.class
+	  };
+	 }
+	 @Override
+	 protected Class < ? > [] getServletConfigClasses() 
+	 {
+	  return null;
+	 }
+	 @Override
+	 protected String[] getServletMappings() 
+	 {
+	  return new String[] {
+	   "/*"
+	  };
+	 }
+}
